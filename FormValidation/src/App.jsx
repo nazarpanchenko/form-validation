ff2c6b4fe1070306147f7b1cf5b7006edec69475
Nazar <panchenko.nazar.a@gmail.com>
2021-02-19 13:05:31 +0200

    init

---
+++ FormValidation/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const App = () => {
+    return (
+        <main>
+            <div className="container"></div>
+        </main>
+    );
+};
+
+export default App;
